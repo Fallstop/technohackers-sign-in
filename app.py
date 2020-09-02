3834793fa5e21be52374f558ed66df2b2c2acdb7
Jasper M-W <jasper@ngita.co.nz>
2020-09-02 20:58:23 +1200

    Repeated attendance warning done

--- app.py
+++ app.py
@@ -50,29 +50,37 @@ def signIn():
     backUpCSVFile.write("\n"+formData["Name"]+","+str(formData["SignedUp"])+","+datetime.datetime.now().isoformat())
     backUpCSVFile.close()
     print("Form values:",(personName,signedUp,datetime.datetime.now().isoformat()))
-    attendance_id = sql_insert_attendance(formData["Name"],formData["SignedUp"])
-    if attendance_id != False:
-        return (attendance_id,200)
+    attendedToday =  False if formData["Force"] else check_attendance(personName)
+    if attendedToday != False:
+        print("Person allready attended")
+        return (jsonify(attendedToday),200)
+    attendanceId = sql_insert_attendance(formData["Name"],formData["SignedUp"])
+    if attendanceId != False:
+        return (jsonify(attendanceId),200)
     else:
         return ("",400)
 
-@app.route('/undo', methods=['DELETE'])
+@app.route('/deleteattendance', methods=['DELETE'])
 def undo_attendance():
     print("Data: ",request.json)
     formData = json.loads(request.data)
-    attendance_id = formData["id"]
+    attendanceId = formData["id"]
+    if (sql_delete_attendance(attendanceId)):
+        return ("",200)
+    else:
+        return ("",400)
 
-def sql_delete_attendance(full_name,registered):
+def sql_delete_attendance(id):
     global connection
     connection.ping(reconnect=True)
     cursor = connection.cursor(cursor=pymysql.cursors.DictCursor)
-    query = "insert into qrl_membership_db.attendance_record (full_name, registered) values (%s,%s);"
+    query = "Delete from attendance_record where attendance_id = (%s);"
     try:
-        cursor.execute(query, (full_name, registered))
+        cursor.execute(query, (id))
         connection.commit()
         return True
     except Exception as e:
-        print("Attendance Insert failed, Error:", e)
+        print("Attendance delete failed, Error:", e)
         return False
 
 def sql_insert_attendance(full_name,registered):
@@ -86,29 +94,53 @@ def sql_insert_attendance(full_name,registered):
         connection.commit()
         id = cursor.lastrowid
         print("Attendance:",id)
-        return str(id)
+        return {
+            "id": id,
+            "attendance_warning": False
+            }
     except Exception as e:
         print("Attendance Insert failed, Error:", e)
         return False
 
-
-def downloadNames():
+def check_attendance(full_name):
+    global connection
+    connection.ping(reconnect=True)
+    cursor = connection.cursor(cursor=pymysql.cursors.DictCursor)
+    query = "select * from attendance_record where arrival_time >= DATE_ADD(CURDATE(), INTERVAL -1 DAY) and full_name = %s order by arrival_time DESC;"
+    
+    try:
+        cursor.execute(query, (full_name))
+        result = cursor.fetchone()
+        print("Check attendance result:",result)
+        if result is not None:
+            return {
+                "id": result["attendance_id"],
+                "full_name": result["full_name"],
+                "previous_time": result["arrival_time"].strftime("%-I:%M %p"),
+                "attendance_warning": True
+            }
+        else:
+            return False
+    except Exception as e:
+        print("Attendance Insert failed, Error:", e)
+        return False
+def downloadYouthNames():
     parsed = []
     try:
         #############
         #SETUP
         #############
-        SCOPES = config.nameListSheet.scopes
-        SPREADSHEET_ID = config.nameListSheet.spreadsheet_id
+        SCOPES = config.youthNameSheet.scopes
+        SPREADSHEET_ID = config.youthNameSheet.spreadsheet_id
 
-        RangeName = config.nameListSheet.range_name
+        RangeName = config.youthNameSheet.range_name
         creds = None
 
         # The file token.pickle stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        if os.path.exists(config.nameListSheet.pickle_name):
-            with open(config.nameListSheet.pickle_name, 'rb') as token:
+        if os.path.exists(config.youthNameSheet.pickle_name):
+            with open(config.youthNameSheet.pickle_name, 'rb') as token:
                 creds = pickle.load(token)
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
@@ -116,10 +148,10 @@ def downloadNames():
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    config.nameListSheet.credentials_name, SCOPES)
+                    config.youthNameSheet.credentials_name, SCOPES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open(config.nameListSheet.pickle_name, 'wb') as token:
+            with open(config.youthNameSheet.pickle_name, 'wb') as token:
                 pickle.dump(creds, token)
         service = build('sheets', 'v4', credentials=creds)
         # Call the Sheets API
@@ -130,10 +162,55 @@ def downloadNames():
     except Exception:
         print("Failed to get google sheet, trying again in 5 seconds")
         time.sleep(5)
-        downloadNames()
+        downloadYouthNames()
+    return parsed
+
+def downloadFacilitatorsNames():
+    parsed = []
+    try:
+        #############
+        #SETUP
+        #############
+        SCOPES = config.facilitatorsNameSheet.scopes
+        SPREADSHEET_ID = config.facilitatorsNameSheet.spreadsheet_id
+
+        RangeName = config.facilitatorsNameSheet.range_name
+        creds = None
+
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists(config.facilitatorsNameSheet.pickle_name):
+            with open(config.facilitatorsNameSheet.pickle_name, 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    config.facilitatorsNameSheet.credentials_name, SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(config.facilitatorsNameSheet.pickle_name, 'wb') as token:
+                pickle.dump(creds, token)
+        service = build('sheets', 'v4', credentials=creds)
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,range=RangeName).execute()["values"]
+        parsed = []
+        for name in result: parsed.append(name[0])
+    except Exception:
+        print("Failed to get google sheet, trying again in 5 seconds")
+        time.sleep(5)
+        downloadFacilitatorsNames()
     return parsed
 
 
 
 print("Getting names from the Google Sheet")
-listOfNames = downloadNames()
+print("Downloading youth names...")
+listOfNames = downloadYouthNames()
+print("Downloading facilitator names...")
+listOfNames += downloadFacilitatorsNames()
+print("Name retrival complete.")
--- static/home.js
+++ static/home.js
@@ -2,8 +2,9 @@ var nameList = [];
 const options = {
     includeScore: true,
     threshold: 0.3
-  }
-  
+}
+var repeatAttendPersonDetails;
+
 var fuse;
 function loadNames() {
     $.ajax({
@@ -15,8 +16,17 @@ function loadNames() {
             fuse = new Fuse(response, options);
             $('#loading_icon').hide();
         },
-        error: function(){
-            loadNames();
+        error: function (response) {
+            $.notify({
+                // options
+                message: "Failed to get list of names from server, response: " + response.status + "<br>Trying again in 2s."
+            }, {
+                // settings
+                type: 'danger',
+                delay: 2000
+            });
+
+            setTimeout("loadNames()", 2000);
         }
     });
 }
@@ -26,48 +36,128 @@ function displayNames(namesToDisplay) {
     console.log("displaying names")
     var htmlResults = "";
     for (var i = 0; i < namesToDisplay.length; i++) {
-        htmlResults += "<div id='person-"+namesToDisplay[i]["item"]+"' class='person' tabindex='"+(i+2)+"'>" + namesToDisplay[i]["item"] + "</div>";
+        htmlResults += "<div id='person-" + namesToDisplay[i]["item"] + "' class='person' tabindex='" + (i + 2) + "'>" + namesToDisplay[i]["item"] + "</div>";
     }
-    htmlResults += "<div id='guest' class='person guest' tabindex='"+(i+2)+"'>Sign in as a guest</div>";
+    htmlResults += "<div id='guest' class='person guest' tabindex='" + (i + 2) + "'>Sign in as a guest</div>";
     $('#search-results-container').html(htmlResults);
 }
 
-function signInPerson(nameOfPerson,signedUp){
+function signInPerson(nameOfPerson, signedUp, force) {
     console.log("Signing in " + nameOfPerson);
-    console.log("Posting: "+JSON.stringify({"Name":nameOfPerson,"SignedUp":signedUp}));
+    data = { "Name": nameOfPerson, "SignedUp": signedUp, "Force": force };
+    console.log("Posting: ", data);
     $.ajax({
         url: '/signin',
         type: 'POST',
         contentType: 'application/json',
-        data: JSON.stringify({"Name":nameOfPerson,"SignedUp":signedUp}),
+        data: JSON.stringify(data),
         success: function (response) {
-            $("#search-input").val("");
-            $('#search-results-container').html("");
-            $('#search-input').focus();
-            rollbackList.push(response);
-            console.log(rollbackList)
+            console.log("Response: ", response);
+            if (response["attendance_warning"]) {
+                repeatAttendPersonDetails = response
+                repeatAttendPersonDetails["signedUp"] = signedUp;
+                $("#repeatAttendPersonDetails-time").html("<strong>"+repeatAttendPersonDetails["previous_time"]+"</strong>");
+                $("#repeatAttendPersonDetails").modal('show');
+            } else {
+                $("#search-input").val("");
+                $('#search-results-container').html("");
+                $('#search-input').focus();
+                rollbackList.push([response["id"], nameOfPerson]);
+                console.log(rollbackList)
+                $.notify({
+                    // options
+                    message: 'Signed in as <strong>' + nameOfPerson + '</strong>'
+                }, {
+                    // settings
+                    type: 'success',
+                    delay: 1000
+
+                });
+            }
+
         },
-        error: function(response){
-            alert("Failed to send to server, response: "+response.status);
+        error: function (response) {
+            $.notify({
+                // options
+                message: "Failed to send sign in to server, response: " + response.status
+            }, {
+                // settings
+                type: 'danger',
+                delay: 2000
+            });
         }
     })
 }
-function guestSignIn(){
-    signInPerson($('#guest-name-input').val(), false);
+
+function guestSignIn() {
+    signInPerson($('#guest-name-input').val(), false, false);
     $("#guestSignInModal").modal('hide');
 }
-function triggerGuestSignIn(){
+
+function repeatAttendSignIn() {
+    $('#repeatAttendPersonSend').html('Sending...');
+    signInPerson(repeatAttendPersonDetails['full_name'], repeatAttendPersonDetails['signedUp'], true);
+    console.log('Signing in with force from modal');
+    $('#repeatAttendPersonDetails').modal('hide');
+    $('#repeatAttendPersonSend').html('Continue anyway');
+    
+}
+
+function triggerGuestSignIn() {
     console.log("Triggering Guest Sign In");
     $("#guestSignInModal").modal('show');
     $("#guest-name-input").val($('#search-input').val());
     $("#guest-name-input").focus();
+}
+function undoSignIn() {
+    if (rollbackList.length > 0) {
+        userToUndo = rollbackList[rollbackList.length - 1];
+        console.log("Canceling sign in of: ", userToUndo);
+        $.ajax({
+            url: '/deleteattendance',
+            type: 'DELETE',
+            contentType: 'application/json',
+            data: JSON.stringify({ "id": userToUndo[0] }),
+            success: function (response) {
+                $.notify({
+                    // options
+                    message: 'Canceled attendance of <strong>' + userToUndo[1] + '</strong>'
+                }, {
+                    // settings
+                    type: 'success',
+                    delay: 1000
+                });
+                rollbackList.pop();
+            },
+            error: function (response) {
+                $.notify({
+                    // options
+                    message: "Failed to undo sign in to server, response: " + response.status
+                }, {
+                    // settings
+                    type: 'danger',
+                    delay: 2000
+                });
+            }
+        });
+    } else {
+        $.notify({
+            // options
+            message: 'Nothing left to undo.'
+        }, {
+            // settings
+            type: 'warning',
+            delay: 2500
+        });
+    }
+
 }
 
 $(document).ready(function () {
     $("#search-input").val("");
     $('#search-input').focus();
     loadNames();
-    $('#search-input').keyup(function(e){
+    $('#search-input').keyup(function (e) {
         console.log(e);
         if (e.code != "Enter") {
             console.log($('#search-input').val());
@@ -76,28 +166,28 @@ $(document).ready(function () {
             } else {
                 $('#search-results-container').html('');
             }
-            
+
         } else {
             var search = fuse.search($('#search-input').val())
             if (search.length > 0) {
-                signInPerson(search[0]["item"], true)
+                signInPerson(search[0]["item"], true, false)
             } else if ($('#search-input').val() != "") {
                 triggerGuestSignIn();
             }
         }
     });
-    $('#guest-name-input').keyup(function(e) {
+    $('#guest-name-input').keyup(function (e) {
         if (e.code == 'Enter') {
             guestSignIn();
         }
     })
-    $(document).on('click','.person',function(event){
+    $(document).on('click', '.person', function (event) {
         if (this.id != "guest") {
-            signInPerson(this.id.substring(7),true);
+            signInPerson(this.id.substring(7), true, false);
         } else {
             triggerGuestSignIn();
         }
-        
+
     });
 
 })
